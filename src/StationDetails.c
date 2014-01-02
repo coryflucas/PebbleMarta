4f50a6bc43e43905ede4f2623be14b39729bf029
Cory Lucas <cory@corylucas.com>
2014-01-01 19:17:31 -0500

    Working JS to make rest call to MARTA service

--- src/StationDetails.c
+++ src/StationDetails.c
@@ -5,6 +5,30 @@ static TextLayer *text_layer;
 
 static const char *station_name;
 
+enum {
+  FETCH_TRAINS = 0x0,
+  STATION_NAME = 0x1,
+  TRAIN_INFO = 0x2,
+};
+
+static void fetch_trains(void) {
+  Tuplet fetch_tupple = TupletInteger(FETCH_TRAINS, 1);
+  Tuplet train_name_tupplet = TupletCString(STATION_NAME, station_name);
+
+  DictionaryIterator *iter;
+  app_message_outbox_begin(&iter);
+
+  if(iter == NULL) {
+    return;
+  }
+
+  dict_write_tuplet(iter, &fetch_tupple);
+  dict_write_tuplet(iter, &train_name_tupplet);
+  dict_write_end(iter);
+
+  app_message_outbox_send();
+}
+
 static void window_load(Window *window) {
   APP_LOG(APP_LOG_LEVEL_DEBUG, "Station details load");
   Layer *window_layer = window_get_root_layer(window);
@@ -14,11 +38,30 @@ static void window_load(Window *window) {
   text_layer_set_text(text_layer, station_name);
   text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(text_layer));
+
+  fetch_trains();
 }
 
 static void window_unload(Window *window) {
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Station details unload");
   text_layer_destroy(text_layer);
+}
+
+static void in_recieved_handler(DictionaryIterator *iter, void *context) {
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "App message recieved");
+  Tuple *train_tuple = dict_find(iter, TRAIN_INFO);
+
+  if(train_tuple) {
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Train info: %s!", train_tuple->value->cstring);
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "length: %d", strlen(train_tuple->value->cstring) );
+  }
+}
+
+static void in_dropped_handler(AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "App message dropped %d", reason);
+}
+
+static void out_failed_handler(DictionaryIterator *failed, AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "App message failed to send %d", reason);
 }
 
 void station_details_init(void) {
@@ -27,6 +70,11 @@ void station_details_init(void) {
     .load = window_load,
     .unload = window_unload,
   });
+
+  app_message_register_inbox_received(in_recieved_handler);
+  app_message_register_inbox_dropped(in_dropped_handler);
+  app_message_register_outbox_failed(out_failed_handler);
+  app_message_open(256, 64);
 }
 
 void station_details_deinit(void) {
--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,5 +1,84 @@
+function compare(x, y) {
+    if(x < y)
+        return -1
+    if(x > y)
+        return 1
+    return 0;
+}
+
+function getUniqueRoutes(response) {
+	return response
+		.map(function (train) {
+		    return {
+		        name: train.ROUTE,
+		        direction: train.DIRECTION
+		    };
+		}).sort(function(x, y) {
+		    return compare(x.name, y.name);
+		}).filter(function (val, idx, arry) {
+		    return idx == 0 || val.direction != arry[idx - 1].direction;
+		});
+}
+
+function getTrains(response) {
+	return getUniqueRoutes(response)
+		.map(function(route) {
+	    	var trainsForLine = response.filter(function(train) {
+	       		return train.ROUTE == route.name
+	            	&& train.DIRECTION == route.direction;
+    		});
+	    
+		    return {
+		        name: route.name[0],
+		        direction: route.direction,
+		        destination: trainsForLine[0].HEAD_SIGN.trim(),
+		        waitTimes: trainsForLine.map(function(train) {
+		            return train.WAITING_TIME;
+		        })
+		    };
+		});
+}
+
+/* 
+ * This is all a little yucky, but its to shrink down the size of the message
+ * we send back to the pebble.
+ */
+function getTrainsForStation(stationName) {
+    console.log("Getting data for " + stationName);
+    var req = new XMLHttpRequest();
+    req.open('GET', 'http://developer.itsmarta.com/NextTrainService/RestServiceNextTrain.svc/GetNextTrain/' + stationName, true);
+    req.onload = function(e) {
+        if (req.readyState == 4) {
+            if(req.status == 200) {
+                var response = JSON.parse(req.responseText);
+
+                var trains = getTrains(response)
+                	.map(function(rnt) {
+				    	var timeStr = rnt.waitTimes.join(',');
+				    	return [rnt.name, rnt.direction, timeStr].join(';');
+					}).join('|');
+				console.log("Message to send: " + trains);
+				Pebble.sendAppMessage({
+					"trainInfo": trains
+				});
+            } else { console.log("Error"); }
+        }
+    }
+    req.send(null);
+}
+
 Pebble.addEventListener("ready",
     function(e) {
         console.log("Hello world! - Sent from your javascript application.");
     }
+);
+
+Pebble.addEventListener("appmessage",
+	function(e) {
+		console.log("Recieved a message");
+		if(e.payload.fetchTrains) {
+			console.log("Fetching trains for station " + e.payload.stationName);
+			getTrainsForStation(e.payload.stationName);
+		}
+	}
 );
